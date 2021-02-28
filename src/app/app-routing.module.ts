2264c85e3d40212024e421a30946aff2943d6d44
Milton F Catano (TMNA) <milton.catano@toyota.com>
2021-02-27 19:04:55 -0500

    Shared module has been created Layout component has been created Home component has been created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(childModule => childModule.HomeModule)
+      },
+      { path: '**', redirectTo: 'home' }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
