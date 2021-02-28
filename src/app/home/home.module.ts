8b4e5ded7a99d1f02582c4395c9ada7cf042c3d3
Milton F Catano (TMNA) <milton.catano@toyota.com>
2021-02-27 19:19:42 -0500

    Home components have been created

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeHeaderComponent } from './home/home-header/home-header.component';
+import { HomeInfoComponent } from './home/home-info/home-info.component';
+import { HomeContentComponent } from './home/home-content/home-content.component';
+import { HomeContactComponent } from './home/home-contact/home-contact.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    HomeHeaderComponent,
+    HomeInfoComponent,
+    HomeContentComponent,
+    HomeContactComponent,
   ],
   imports: [
     CommonModule,
---
+++ src/app/home/home/home-content/home-content.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-content',
+  templateUrl: './home-content.component.html',
+  styleUrls: ['./home-content.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
