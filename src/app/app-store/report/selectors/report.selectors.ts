f8329fc5da365b57bb36be946f9ff8f259366c07
Milton F Catano (TMNA) <milton.catano@toyota.com>
2021-03-01 11:26:32 -0500

    Remove angular project

--- src/app/app-store/report/selectors/report.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from '@ngrx/store';
-import { reportAdapter, reportFeatureSelector } from '../report.store';
-
-const { selectAll } = reportAdapter.getSelectors();
-
-export const selectGetAllReporters = createSelector(reportFeatureSelector, selectAll);
-
-export const selectLoadingGetAllReports = createSelector(reportFeatureSelector, (state) => state.loadingGetAllReports);
-export const selectLoadingUpdateLikeUnlikeReport = createSelector(reportFeatureSelector, (state) => state.loadingUpdateLikeUnlikeReport);
--- src/app/shared/models/report.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Report {
-  id: string;
-  img: string;
-  title: string;
-  subTitle: string;
-  description: string;
-  like: number;
-  unlike: number;
-}
