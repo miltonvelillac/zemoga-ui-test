832e35a4cb613552c06b0d22e035261aaa19b625
Milton F Catano (TMNA) <milton.catano@toyota.com>
2021-03-01 14:04:03 -0500

    navbar design has been created

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
